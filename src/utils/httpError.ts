a1ddc3497796689c7d2c34a0ecc8d18e2317d956
xquery <best.highloyal@gmail.com>
2024-06-21 10:57:35 +0200

    Create custom error class

---
+++ src/utils/httpError.ts
@@ -0,0 +1,13 @@
+
+class HttpError extends Error {
+  status: number;
+  code: string;
+  constructor(status: number, code: string, message: string) {
+    super(message);
+    this.status = status;
+    this.code = code;
+    this.message = message;
+  }
+}
+
+export default HttpError;
