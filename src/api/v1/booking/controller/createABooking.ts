9dca3df0a0db98fdd9cc110fa6216c0a4b97113c
xquery <best.highloyal@gmail.com>
2024-06-29 11:05:15 +0200

    Send created bookings as a response

--- src/api/v1/booking/controller/createABooking.ts
+++ src/api/v1/booking/controller/createABooking.ts
@@ -1,15 +1,21 @@
 import asyncHandeler from "../../../../utils/asyncHandeler";
-import bookingService from '../../../../lib/booking'
+import bookingService from "../../../../lib/booking";
 
-const createABooking = asyncHandeler(async(req,res)=>{
-     // destructure userId and bookingInfo from req
-     const userId = req.user.paylode.id;
-     const bookingInfo = req.body
-   
-     // Call CreateBooking service with needed argument
-   const bookings = await bookingService.createABooking(userId,bookingInfo)
-   console.log(bookings)
+const createABooking = asyncHandeler(async (req, res) => {
+  // destructure userId and bookingInfo from req
+  const userId = req.user.paylode.id;
+  const bookingInfo = req.body;
 
-})
+  // Call CreateBooking service with needed argument
+  const bookings = await bookingService.createABooking(userId, bookingInfo);
 
-export default createABooking
+  // send Created bookings as a response
+  res.status(201).json({
+    success: true,
+    statusCode: 201,
+    message: "Booking created successfully",
+    data: bookings,
+  });
+});
+
+export default createABooking;
