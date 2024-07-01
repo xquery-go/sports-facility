fbc917472831bd82fc6082ebaf5dea8448f289c0
xquery <best.highloyal@gmail.com>
2024-07-01 12:33:25 +0200

    formet code

--- src/api/v1/booking/controller/getAllBookings.ts
+++ src/api/v1/booking/controller/getAllBookings.ts
@@ -1,9 +1,9 @@
 import asyncHandeler from "../../../../utils/asyncHandeler";
 import bookingsService from "../../../../lib/booking";
-const getAllBookings = asyncHandeler(async (_req, res) => {
 
-    // Call getAllBookings service
-  const bookings =await bookingsService.getAllBookings();
+const getAllBookings = asyncHandeler(async (_req, res) => {
+  // Call getAllBookings service
+  const bookings = await bookingsService.getAllBookings();
 
   // Send all retrived bookings as a response
   res.status(200).json({
