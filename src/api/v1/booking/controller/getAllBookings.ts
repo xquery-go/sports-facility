ea3fe8b55f23533f46bfcdaf790990c7fe3e72b6
xquery <best.highloyal@gmail.com>
2024-07-01 12:32:43 +0200

    retrive all bookins and send as a reponse

--- src/api/v1/booking/controller/getAllBookings.ts
+++ src/api/v1/booking/controller/getAllBookings.ts
@@ -1,13 +1,17 @@
 import asyncHandeler from "../../../../utils/asyncHandeler";
-import bookingsService from '../../../../lib/booking'
-const getAllBookings = asyncHandeler(async(_req,res)=>{
-   
+import bookingsService from "../../../../lib/booking";
+const getAllBookings = asyncHandeler(async (_req, res) => {
 
-    
-    
+    // Call getAllBookings service
+  const bookings =await bookingsService.getAllBookings();
 
-
+  // Send all retrived bookings as a response
+  res.status(200).json({
+    success: true,
+    statusCode: 200,
+    message: "Bookings retrieved successfully",
+    data: bookings,
+  });
 });
-
 
 export default getAllBookings;
